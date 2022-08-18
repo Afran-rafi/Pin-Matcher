7c6c0b728bf89deb7e93419e9e6d2ecc27a2797e
Afran-rafi <fahimrrrrr@gmail.com>
2022-08-19 01:28:08 +0600

    verifyPin by my way and jhankar sir way

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,4 @@
-
-//  addEventListener way ----------->
-/* 
-document.getElementById('generate-pin-btn').addEventListener('click', function () {
-    const random = Math.round(Math.random() * 10000);
-    const newRandom = random + ''
-    console.log(newRandom)
-    const input = document.getElementById('generate-input');
-    const inputValue = input.value;
-    if (newRandom.length == 4) {
-        parseInt(newRandom);
-        input.value = newRandom
-    }
-})
-*/
-
-// function way ------------>
+// function way ------------> Jhankar Mahbub Sir ------->
 
 function getPin() {
     const random = Math.round(Math.random() * 10000);
@@ -31,3 +15,83 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('generate-input').value = pin
 }
+
+document.getElementById('digit').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const input = document.getElementById('number-input');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            input.value = '';
+        }
+    }
+    else {
+        const inputValue = input.value;
+        const newNumber = inputValue + number;
+        input.value = newNumber;
+    }
+});
+
+function verifyPin() {
+    const randomPin = document.getElementById('generate-input').value
+    const myPin = document.getElementById('number-input').value
+    const success = document.getElementById('success')
+    const fail = document.getElementById('fail')
+    if (randomPin == myPin) {
+        success.style.display = 'block';
+        fail.style.display = 'none';
+    }
+    else {
+        success.style.display = 'none';
+        fail.style.display = 'block';
+    }
+}
+
+
+//  addEventListener way -----------> Fahim Ahmed ------->
+
+/*
+document.getElementById('generate-pin-btn').addEventListener('click', function () {
+    const random = Math.round(Math.random() * 10000);
+    const newRandom = random + ''
+    console.log(newRandom)
+    const input = document.getElementById('generate-input');
+    const inputValue = input.value;
+    if (newRandom.length == 4) {
+        parseInt(newRandom);
+        input.value = newRandom
+    }
+})
+
+const digits = document.getElementsByClassName('button');
+for (const digit of digits) {
+    digit.addEventListener('click', function (e) {
+        const number = e.target.innerText;
+        const input = document.getElementById('number-input');
+        if (isNaN(number)) {
+            if (number == 'C') {
+                input.value = ''
+            }
+        }
+        else {
+            const inputValue = input.value;
+            const newNumber = inputValue + number;
+            input.value = newNumber;
+        }
+    })
+}
+
+document.getElementById('submit-btn').addEventListener('click', function () {
+    const randomPin = document.getElementById('generate-input')
+    const myPin = document.getElementById('number-input')
+    const success = document.getElementById('success')
+    const fail = document.getElementById('fail')
+    if (randomPin.value == myPin.value) {
+        fail.style.display = 'none';
+    }
+    else {
+        success.style.display = 'none'
+    }
+})
+*/
+
+
